056ceea8c0f74bf64454573fbdaecf7ab27d9fce
Mike Collins <mike.d.collins@gmail.com>
2016-05-09 10:08:01 -0700

    fiddle with restify.serveStatic, not working

--- app.js
+++ app.js
@@ -31,11 +31,45 @@ helloBot.add('/profile',  [
 server.use(helloBot.verifyBotFramework({ appId: 'Ihavenoideawhattoputhere', appSecret: '1d29571daa80409494745524a5ecaf45' }));
 server.post('/v1/messages', helloBot.listen());
 
+function send(req, res, next) {
+    console.log('send: req.params.name is %s', req.params.name);
+    if (req.params.name === 'x') {
+        console.log('req.params.name is x serve page');
+        return restify.serveStatic({
+            'directory': './html/Support',
+            'default': 'Slack.html'
+        })
+    }
+    else {
+        res.send('not x' + req);
+        return next();
+    }
+ }
+ 
+//server.get('/X:name', send);
+
+// !!!!!!! this is not working, can't figure out
+// how serverStatic works...
+//
+//function(req, res, next) {
+//  console.log(req.params[0]);
+//  console.log(req.params[1]);
+server.get(/\/Support\/?.*/, restify.serveStatic({
+            'directory': './html/Support',
+            'default': 'Slack.html'})
+//   res.send(200);
+//   return next();
+)
+
+
+
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
+       'directory': './html',
+	    'default': 'index.html'
+   })
+//   return next();
+);
 
 server.listen(process.env.port, function () {
     console.log('%s listening to %s', server.name, server.url); 
